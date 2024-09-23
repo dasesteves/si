fa330558b66bac94bdbbd7700c50b9e91f7c2eb1
dasesteves <dasesteves@hotmail.com>
2024-09-23 10:19:03 +0100

    Adding my co-authorship to the package

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "Diogo Esteves"
 __credits__ = ["Diogo Esteves"]
 __license__ = "Apache License 2.0"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __maintainer__ = "Diogo Esteves"
 __email__ = "dasesteves@hotmail.com"
 
