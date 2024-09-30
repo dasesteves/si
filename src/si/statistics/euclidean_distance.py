a446055b80a229aee1845b177b2b70ab1fcaefc6
dasesteves <dasesteves@hotmail.com>
2024-09-30 11:54:59 +0100

    euclidean_distance added

---
+++ src/si/statistics/euclidean_distance.py
@@ -0,0 +1,15 @@
+def euclidean_distance
+'''• arguments:
+- x – a single sample
+- y – multiple samples
+• expected ouput:
+- an array containing the distances between X and the various
+samples in Y.
+• algorithm
+- calculates the Euclidean distance between X and Y using the
+following formula:
+• distance_x_y1 = np.sqrt((x1 - y11)^2 + (x2 - y12)^2 + ... + (xn -
+y1n)^2)
+• distance_x_y2 = np. sqrt((x1 - y21)^2 + (x2 - y22)^2 + ... + (xn -
+y2n)^2)
+'''
